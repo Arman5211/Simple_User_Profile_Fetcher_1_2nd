872edce5824675c738259838d97497adc6d54115
Arman <armanhossain3584@gmail.com>
2024-11-07 10:55:48 +0600

    Initial commit

---
+++ app/src/main/java/com/example/simpleuserprofilefetcher/UserViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.simpleuserprofilefetcher
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.simpleuserprofilefetcher.RetrofitInstance
+import com.example.simpleuserprofilefetcher.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class UserViewModel : ViewModel() {
+    val user = MutableLiveData<User>()
+    val errorMessage = MutableLiveData<String>()
+
+    fun fetchUserProfile() {
+        RetrofitInstance.api.getUserProfile().enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    user.value = response.body()
+                } else {
+                    errorMessage.value = "Failed to load data"
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                errorMessage.value = "Error: ${t.message}"
+            }
+        })
+    }
+}
+
